8cebdc75555eaf0c2c6d51159c246a20ab9115af
Paul T <developer.paul.123@gmail.com>
2024-04-01 09:22:22 -0400

    chore: read version from toml when installing package locally
    Updated script to read the package version from the toml file

--- scripts/install_package_locally.ps1
+++ scripts/install_package_locally.ps1
@@ -1,3 +1,10 @@
+# requires the PSToml cmdlet to be installed
+# https://github.com/jborean93/PSToml?tab=readme-ov-file
+# Install-Module -Name PSToml -Scope AllUsers
+
+$typst_toml = ConvertFrom-Toml (Get-Content "$PSScriptRoot/../typst.toml" -Raw)
+Write-Host 'Package version: ' $typst_toml.package.version
 $Source = "$PSScriptRoot/../*"
-$Destination = "$env:LOCALAPPDATA/typst/packages/local/modern-cv/0.1.0"
+$Destination = "$env:LOCALAPPDATA/typst/packages/local/modern-cv/$($typst_toml.package.version)"
+New-Item -ItemType Directory -Path $Destination -Force
 Copy-Item -Path $Source -Destination $Destination -Recurse -Force
