3b7c1f45c1629cf8062b2d7dbe642f1ce9c98246
Paul T <developer.paul.123@gmail.com>
2024-04-02 17:53:36 -0400

    feature: add another coverletter example

--- scripts/update_thumbnails.ps1
+++ scripts/update_thumbnails.ps1
@@ -1,4 +1,5 @@
 typst compile -f png $PSScriptRoot/../template/resume.typ $PSScriptRoot/../assets/images/resume.png
 typst compile -f png $PSScriptRoot/../template/coverletter.typ $PSScriptRoot/../assets/images/coverletter.png
+typst compile -f png $PSScriptRoot/../template/coverletter2.typ $PSScriptRoot/../assets/images/coverletter2.png
 
 oxipng.exe $PSScriptRoot/../assets/images/*
