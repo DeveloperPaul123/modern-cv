caa0618fdb07a1a955703a3137ec2834b9be3aee
Paul Tsouchlos <developer.paul.123@gmail.com>
2025-05-25 11:57:14 -0400

    fix: update thumbnails script
    Only process pngs with `oxipng`

--- scripts/update_thumbnails.ps1
+++ scripts/update_thumbnails.ps1
@@ -2,4 +2,4 @@ typst compile -f png $PSScriptRoot/../template/resume.typ $PSScriptRoot/../asset
 typst compile -f png $PSScriptRoot/../template/coverletter.typ $PSScriptRoot/../assets/images/coverletter.png
 typst compile -f png $PSScriptRoot/../template/coverletter2.typ $PSScriptRoot/../assets/images/coverletter2.png
 
-oxipng.exe $PSScriptRoot/../assets/images/*
+oxipng.exe $PSScriptRoot/../assets/images/*.png
